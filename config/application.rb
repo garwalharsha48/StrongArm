53cfabefc15e1714a5c4f6a94d80d697cca098ce
melissawahnish <melissawahnish@gmail.com>
2015-01-09 14:52:54 -0500

    Remove fat_free stuff not needed

--- config/application.rb
+++ config/application.rb
@@ -17,9 +17,6 @@ module Strongarm
       g.helper_specs false
     end
 
-    Railties.engines.each do |engine|
-      config.paths['db/migrate'] += engine.paths['db/migrate'].existent
-    end
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
