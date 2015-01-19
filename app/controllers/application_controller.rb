979e772f4db4991f6a4b40e64f94e6587c762c8a
melissawahnish <melissawahnish@gmail.com>
2015-01-19 14:21:28 -0500

    Take out password proctection for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :authenticate_staging_access
+  # before_filter :authenticate_staging_access
 
-  def authenticate_staging_access
-    if Rails.env.staging?
-      authenticate_or_request_with_http_basic do |username, password|
-        username == 'strongarmtech' && password == 'strongarm'
-      end
-    end
-  end
+  # def authenticate_staging_access
+  #   if Rails.env.staging?
+  #     authenticate_or_request_with_http_basic do |username, password|
+  #       username == 'strongarmtech' && password == 'strongarm'
+  #     end
+  #   end
+  # end
 end
