11de31687f820e8740404793e1ad84561760e0f2
melissawahnish <melissawahnish@gmail.com>
2015-01-19 19:58:43 -0500

    Trying dotev for secret key base

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  # before_filter :authenticate_staging_access
-
-  # def authenticate_staging_access
-  #   if Rails.env.staging?
-  #     authenticate_or_request_with_http_basic do |username, password|
-  #       username == ENV['STAGING_USERNAME'] && password == ENV['STAGING_PASSWORD']
-  #     end
-  #   end
-  # end
 end
