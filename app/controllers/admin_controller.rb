2dc573bd751f6447bc9cfc09e492003c12d2d9e0
melissawahnish <melissawahnish@gmail.com>
2015-05-27 15:35:41 -0400

    Add downloading csv to newsletter emails
    make dashboard readable

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,6 +4,13 @@ class AdminController < ApplicationController
 	
   def dashboard
   	@newsletter_emails = NewsletterEmail.all
+  	respond_to do |format|
+	    format.html
+	    format.csv do
+	      headers['Content-Disposition'] = "attachment; filename=\"mailing-list\""
+	      headers['Content-Type'] ||= 'text/csv'
+	    end
+	  end
   end
 
   def newsletter
