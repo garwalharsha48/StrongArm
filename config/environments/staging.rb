7dcba71e55223f114d264c0f5fdbd47b7c341a1e
melissawahnish <melissawahnish@gmail.com>
2015-06-02 20:59:55 -0400

    Update ENV for secretsl.yml and Sendgrid

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -83,12 +83,12 @@ Rails.application.configure do
   end
 
   ActionMailer::Base.smtp_settings = {
-    :user_name => '<%= ENV["SENDGRID_USERNAME"] %>',
-    :password => '<%= ENV["SENDGRID_PASSWORD"] %>',
-    :domain => 'strongarmtech.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    user_name:            ENV["SENDGRID_USERNAME"],
+    password:             ENV["SENDGRID_PASSWORD"],
+    domain:               'strongarmtech.com',
+    address:              'smtp.sendgrid.net',
+    port:                 587,
+    authentication:       :plain,
+    enable_starttls_auto: true
   }
 end
